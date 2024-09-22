674ccc741721e5810416f9b51f5c9f657f192843
Bohdan Holubnychyi <115166530+Dakose@users.noreply.github.com>
2024-09-23 01:36:21 +0300

    profile settings and edit profile page

--- webapp/members/urls.py
+++ webapp/members/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import UserRegisterView, UserEditView, PasswordsChangeView, password_success
+from .views import UserRegisterView, UserEditView, PasswordsChangeView, ShowProfilePageView, EditProfilePageView,password_success
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
-    path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
+    path('edit_settings/', UserEditView.as_view(), name='edit_settings'),
     path('password/', PasswordsChangeView.as_view(template_name="registration/change-password.html")),
     path('password_success', password_success, name='password_success'),
-    
+    path('<int:pk>/profile/', ShowProfilePageView.as_view(), name='show_profile_page'),
+    path('<int:pk>/edit_profile_page/', EditProfilePageView.as_view(), name='edit_profile_page'),
 ]
